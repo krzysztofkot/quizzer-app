a5067cbddc722e7091714f0d063f4dc91de7af1d
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 22:21:47 +0100

    Add register form

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -1,2 +1,14 @@
 <x-layout>
+    <x-utility.page-heading>
+        First time? Please register!
+    </x-utility.page-heading>
+    <div class="rounded-md p-4 max-w-[480px] mx-auto bg-white mt-12">
+        <x-forms.form method="post" action="/register" class="flex flex-col gap-y-4">
+            <x-forms.text-field name="name" label="Name" />
+            <x-forms.text-field name="email" label="E-mail" type="email" />
+            <x-forms.text-field name="password" label="Password" type="password" />
+            <x-forms.text-field name="password_confirmation" label="Password Confirmation" />
+            <x-forms.button>Register</x-forms.button>
+        </x-forms.form>
+    </div>
 </x-layout>
