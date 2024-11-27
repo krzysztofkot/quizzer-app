b9f0ab88a687b385fa90695076a7bd08966db60b
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 23:11:33 +0100

    Add multiple button types

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,2 +1,16 @@
-<button
-    {{ $attributes(['class' => 'px-3 py-2 rounded-md text-white bg-blue-600 text-lg']) }}>{{ $slot }}</button>
+@props(['type' => 'primary', 'size' => 'big'])
+
+<?php
+$size = match ($size) {
+    'big' => 'px-3 py-2 text-lg',
+    'small' => 'p-1.5 text-sm',
+    default => '',
+};
+
+$type = match ($type) {
+    'primary' => 'rounded-md text-white bg-blue-600',
+    default => '',
+};
+?>
+
+<button {{ $attributes->class([$type, $size]) }}>{{ $slot }}</button>
