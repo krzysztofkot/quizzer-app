670c4c6da1da6221532d2b556e3ccb6a9d02c3e0
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 22:21:28 +0100

    DIsplay proper buttons for logged users

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,24 @@
 <nav
     class="flex justify-between items-center w-full bg-black px-4 md:px-8 xl:px-12 2xl:px-16 py-4 border-b border-gray-600">
     <x-nav.logo />
-    <div class="flex gap-x-4">
-        <x-nav.nav-link href="{{ route('login') }}" class="text-white">
-            Login
-        </x-nav.nav-link>
-        <x-nav.nav-link href="{{ route('register') }}" class="text-white">
-            Register
-        </x-nav.nav-link>
-    </div>
+    @auth
+        <div class="flex gap-x-4">
+            <x-nav.nav-link href="{{ route('dashboard') }}" class="text-white">
+                Go to dashboard
+            </x-nav.nav-link>
+        </div>
+    @endauth
+    @guest
+        <div class="flex gap-x-4">
+            <x-nav.nav-link href="{{ route('login') }}" class="text-white">
+                Login
+            </x-nav.nav-link>
+            <x-nav.nav-link href="{{ route('register') }}" class="text-white">
+                Register
+            </x-nav.nav-link>
+        </div>
+    @endguest
+
 
 
 </nav>
