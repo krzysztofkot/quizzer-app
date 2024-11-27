f82343a5ef6dcdd5c4a1f891a686ef5c7e273839
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 22:23:23 +0100

    Add register user functionality

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -11,8 +13,19 @@ class SessionController extends Controller
         return view('Auth.login');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        return redirect(route('register'));
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard.index');
+        } else {
+            throw ValidationException::withMessages(['email' => 'Incorrect E-mail or/and password']);
+        }
     }
 }
