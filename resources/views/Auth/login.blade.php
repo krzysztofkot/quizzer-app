e0aa0c20b4e378cb027c25d2b783d07a26d76b1e
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 22:19:57 +0100

    Fix text-field names

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -3,9 +3,9 @@
         Welcome back! Please log in
     </x-utility.page-heading>
     <div class="rounded-md p-4 max-w-[480px] mx-auto bg-white mt-12">
-        <x-forms.form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-y-8">
-            <x-forms.text-field name="E-mail" label="E-mail" />
-            <x-forms.text-field name="Password" label="Password" />
+        <x-forms.form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-y-4">
+            <x-forms.text-field name="email" label="E-mail" />
+            <x-forms.text-field name="password" label="Password" />
             <x-forms.button>Log in</x-forms.button>
         </x-forms.form>
     </div>
