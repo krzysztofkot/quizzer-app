02c0cd4c16970c9f73f97119c97c8924cfdcaa82
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 23:10:33 +0100

    Add logout button

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,10 +2,15 @@
     class="flex justify-between items-center w-full bg-black px-4 md:px-8 xl:px-12 2xl:px-16 py-4 border-b border-gray-600">
     <x-nav.logo />
     @auth
-        <div class="flex gap-x-4">
+        <div class="flex gap-x-4 items-center">
             <x-nav.nav-link href="{{ route('dashboard') }}" class="text-white">
                 Go to dashboard
             </x-nav.nav-link>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                @method('DELETE')
+                <x-forms.button size="small">Logout</x-forms.button>
+            </form>
         </div>
     @endauth
     @guest
