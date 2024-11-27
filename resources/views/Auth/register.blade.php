9ef55b4a2a59647a4d6de771a8a0e3b8ed99385c
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 23:10:53 +0100

    Generate proper route name

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -3,7 +3,7 @@
         First time? Please register!
     </x-utility.page-heading>
     <div class="rounded-md p-4 max-w-[480px] mx-auto bg-white mt-12">
-        <x-forms.form method="post" action="/register" class="flex flex-col gap-y-4">
+        <x-forms.form method="POST" action="{{ route('register.store') }}" class="flex flex-col gap-y-4">
             <x-forms.text-field name="name" label="Name" />
             <x-forms.text-field name="email" label="E-mail" type="email" />
             <x-forms.text-field name="password" label="Password" type="password" />
