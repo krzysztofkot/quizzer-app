4720b7538a43434ea95e5385b34306db1eb1b60c
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 22:19:29 +0100

    Add error field to text input

--- resources/views/components/forms/text-field.blade.php
+++ resources/views/components/forms/text-field.blade.php
@@ -8,4 +8,7 @@
     @endif
     <input class="rounded-md border border-gray-200 p-2 bg-gray-100" type="{{ $type }}" id="{{ $name }}"
         name="{{ $name }}" value="{{ old($name) }}" />
+    @if ($errors->first($name))
+        <p class="text-red-700 text-xs mt-1">{{ $errors->first($name) }}</p>
+    @endif
 </div>
