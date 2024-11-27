12e848fe978bcdf6e1d0bb727b419e5b40412573
Krzysztof Kot <kootek2@gmail.com>
2024-11-27 22:19:15 +0100

    Style button component

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,1 +1,2 @@
-<button {{ $attributes(['class' => '']) }}>{{ $slot }}</button>
+<button
+    {{ $attributes(['class' => 'px-3 py-2 rounded-md text-white bg-blue-600 text-lg']) }}>{{ $slot }}</button>
