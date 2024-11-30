423fcb151bf85760bece3fccc760b97fcde21d26
Krzysztof Kot <kootek2@gmail.com>
2024-11-30 22:49:51 +0100

    Pass quizes to view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 class DashboardController extends Controller
 {
     public function index()
     {
+        $user = Auth::user();
 
-        return view('Dashboard.index');
+        $quizes = $user->quizes->all();
+        return view('Dashboard.index', ['quizes' => $quizes]);
     }
 }
