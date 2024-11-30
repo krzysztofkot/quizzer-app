5cabbd1ed90d4e776290d04e01a3c73faa20f404
Krzysztof Kot <kootek2@gmail.com>
2024-11-30 22:49:35 +0100

    Add basing html structure

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -1,3 +1,23 @@
-<x-layout>
-    Dashboard
+<x-layout bg="bg-white">
+    <x-utility.h1>Dashboard</x-utility.h1>
+
+    <div>
+        <x-utility.btn-link href="{{ route('quizes.create') }}" class="flex gap-2 items-center">
+            <span>Add new quiz</span>
+            <img src="{{ Vite::asset('resources/images/icons/PlusIcon.svg') }}" />
+        </x-utility.btn-link>
+    </div>
+    @if (!count($quizes))
+        <section class="mt-4">
+            <div class="mx-auto w-[300px]">
+                <img src="{{ Vite::asset('resources/images/emptyStates/dashboard.png') }}"
+                    class="w-full object-cover object-center" />
+            </div>
+            <x-utility.paragraph class="text-center">You don't have any quizes yet. Maybe it's time to create
+                one?</x-utility.paragraph>
+        </section>
+    @else
+        {{ count($quizes) }} amount
+    @endif
+
 </x-layout>
