bfbb168566829a97eb1a7031296ea73e4273d4d8
Krzysztof Kot <kootek2@gmail.com>
2024-11-30 22:50:48 +0100

    Add routes for creating and saving quizes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RegisterUserController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'Home.index')->name('homepage');
@@ -17,6 +18,8 @@ Route::middleware('guest')->group(function () {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard/quizes', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard/quizes/create', [QuizController::class, 'create'])->name('quizes.create');
+    Route::post('/dashboard/quizes', [QuizController::class, 'store'])->name('quizes.store');
     Route::delete('/logout', [SessionController::class, 'destroy'])->name('logout');
 });
