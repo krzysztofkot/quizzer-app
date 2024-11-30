b87ad1147b365e632204144ad1e7bdbda4d82727
Krzysztof Kot <kootek2@gmail.com>
2024-11-30 22:50:32 +0100

    Add base structure for QuizController

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    public function create()
+    {
+        return view('Quizes.create');
+    }
+
+    public function store(Request $request) {}
+}
