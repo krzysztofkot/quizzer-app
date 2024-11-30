8bf27a894a66ec18dfed30a01633e112668c5c8b
Krzysztof Kot <kootek2@gmail.com>
2024-11-30 21:33:10 +0100

    Add QuizItem model

---
+++ database/migrations/2024_11_27_221952_create_quiz_item_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Quiz;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quiz_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Quiz::class);
+            $table->string('photo_name')->nullable();
+            $table->string('answer_a');
+            $table->string('answer_b');
+            $table->string('answer_c');
+            $table->string('answer_d');
+            $table->string('correct_answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quiz_item');
+    }
+};
