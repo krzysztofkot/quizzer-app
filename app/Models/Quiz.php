585fceea2dce0f3389b7648aaf58685de545b70e
Krzysztof Kot <kootek2@gmail.com>
2024-12-02 21:52:02 +0100

    Use trait in Quiz model

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\GenerateUniqueSlugTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Quiz extends Model
 {
+    use GenerateUniqueSlugTrait;
     protected $table = 'Quizes';
-    protected $fillable = ['title', 'description', 'slug'];
+    protected $fillable = ['title', 'description', 'slug', 'user_id'];
     //
 
     public function user(): BelongsTo
