fed1260e5883a97cf53f6f8ba0b84784cd592412
Krzysztof Kot <kootek2@gmail.com>
2024-12-02 21:52:20 +0100

    Add store Quiz method

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class QuizController extends Controller
 {
@@ -11,5 +14,22 @@ class QuizController extends Controller
         return view('Quizes.create');
     }
 
-    public function store(Request $request) {}
+    public function store(Request $request)
+    {
+        $quiz = $request->validate([
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string', 'max:255']
+        ]);
+
+        $quiz['slug'] = Str::slug($request->input('title'));
+
+        $result = Auth::user()->quizes()->create($quiz);
+
+        return redirect()->route('quizes.show', ['quiz' => $result->id]);
+    }
+
+    public function show(Quiz $quiz)
+    {
+        return view('Quizes.show', ['quiz' => $quiz]);
+    }
 }
